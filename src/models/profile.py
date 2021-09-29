d1a43ad3e848c4776cc6812e2d03f1e7d96e564d
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-29 15:00:53 -0300

    comment column blob for images

--- src/models/profile.py
+++ src/models/profile.py
@@ -6,7 +6,7 @@ class Profile(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), default="")
-    avatar = db.Column(db.BLOB)
+    # avatar = db.Column(db.BLOB)
 
     def serialize(self):
         return {
