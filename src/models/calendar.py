7eba8e90daf7ddab7b6ef88f397d6a8505a1afe6
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-06 09:59:59 -0300

    change fecha to bigint

--- src/models/calendar.py
+++ src/models/calendar.py
@@ -5,6 +5,6 @@ class Fecha(db.Model):
     __tablename__ = 'fechas'
 
     id = db.Column(db.Integer, primary_key=True)
-    fecha = db.Column(db.Integer, unique=False)
+    fecha = db.Column(db.BigInteger, unique=False)
     category = db.Column(db.String(100), unique=False)
     profile = db.Column(db.Integer, db.ForeignKey('profiles.id'))
