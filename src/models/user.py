cb66a98f88dd2ac06fc0a89e3d39d035f0f118f0
Italo Guajardo <italog.penaloza@gmail.com>
2021-11-01 15:19:51 -0300

    change size of id for users and remove bps

--- src/models/user.py
+++ src/models/user.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.String(50), primary_key=True)
+    id = db.Column(db.String(100), primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
